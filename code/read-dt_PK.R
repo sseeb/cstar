81c87fd90968b2374955400f3ea8fa3ee58d7ebf
paigekouba <pvkouba@ucdavis.edu>
2025-04-29 16:10:42 -0700

    notes on data cleaning

--- code/read-dt_PK.R
+++ code/read-dt_PK.R
@@ -1,11 +1,14 @@
 library(sf)      ## spatial data
 library(dplyr)   ## data manipulation
 library(ggplot2) ## graphs
+library(skimr)
+library(sf)
 
 ##--- pre processing ----
 
 ## reading data
 my_dt <- readr::read_csv("data/data.csv")
+# automatically detects the data type per column, fixes dates
 
 ## cleaning column names
 my_dt <- janitor::clean_names(my_dt)
@@ -30,3 +33,34 @@ ggplot(data = my_dt) +
   scale_color_viridis_c(option = "H",
                         trans = "log1p") +
   theme_minimal()
+
+# dropping spatial part of df
+my_dt %>% 
+  st_drop_geometry() %>% # bc we don't need to get summary data for geometry data
+  skimr::skim() # tells us cool stuff about dataset
+
+skim(cars)
+
+my_dt <- my_dt %>% 
+  mutate(species_1 = toupper(species_1),
+         species_2 = toupper(species_2))
+
+
+## merging species with "UNID" in their name
+my_dt <- my_dt |>
+  mutate(species_p_1 = if_else(grepl("(^U|UNID)", species_1), "UNID", species_1),
+         species_p_2 = if_else(grepl("(^U|UNID)", species_2), "UNID", species_2))
+# hat says "something that starts with", bar says "or"
+
+my_dt |>
+  st_drop_geometry() |>
+  count(species_p_1) |> # you can also count by multiple variables
+  arrange(-n) |> # arrange in descending order of count
+  mutate(pc = 100 * n / sum(n)) |>
+  print(n = Inf) # so it prints the whole table
+
+my_dt |>
+  st_drop_geometry() |>
+  count(event_code) |>
+  arrange(-n) |>
+  print(n = Inf)
